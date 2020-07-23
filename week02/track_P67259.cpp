15acb09aa52aa34360e2cfb1861124b6f72e59fd
easymean <m2162003@gmail.com>
2020-07-23 16:04:09 +0900

    fix track

--- week02/track_P67259.cpp
+++ week02/track_P67259.cpp
@@ -12,7 +12,7 @@ int dy[4] = {0, 0, -1, 1};
 int n;
 int min_pay = INT_MAX;
 
-int copy_board[25][25][2] = {
+int copy_board[25][25] = {
     0,
 }; // 0은 직선도로, 1은 코너
 
@@ -27,32 +27,23 @@ bool isValid(int row, int col)
 
 void print(void)
 {
-  printf("zero\n");
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      printf("%5d  ", copy_board[i][j][0]);
+      printf("%5d  ", copy_board[i][j]);
     }
     printf("\n");
   }
-  printf("one\n");
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      printf("%5d  ", copy_board[i][j][0]);
-    }
-    printf("\n");
-  }
+
   printf("\n");
 }
 
 void BFS(vector<vector<int>> board)
 {
-  queue<tuple<int, int, int, int>> q;
+  queue<tuple<int, int, int>> q;
   //row, col, 방향 저장: 상하 0 좌우 1
-  q.push(make_tuple(0, 0, 3, 0));
+  q.push(make_tuple(0, 0, 3));
   int tmp_pay = 0;
 
   while (!q.empty())
@@ -60,7 +51,6 @@ void BFS(vector<vector<int>> board)
     int cur_x = get<0>(q.front());
     int cur_y = get<1>(q.front());
     int cur_dir = get<2>(q.front());
-    int cur_state = get<3>(q.front());
     q.pop();
 
     for (int i = 0; i < 4; i++)
@@ -68,53 +58,40 @@ void BFS(vector<vector<int>> board)
       int next_x = cur_x + dx[i];
       int next_y = cur_y + dy[i];
       int next_dir;
-      int next_state;
 
-      if (!isValid(next_x, next_y) || (next_x == 0 && next_y == 0))
+      if (!isValid(next_x, next_y) || (next_x == 0 && next_y == 0) || board[next_x][next_y] != 0)
       {
         continue;
       }
       next_dir = i / 2; //방향 설정
+      int tmp;
       if (cur_dir == 3)
       {
-        next_state = 0;
+        tmp = copy_board[cur_x][cur_y] + 100;
       }
       else
       {
         if (cur_dir == next_dir)
         {
-          next_state = 0;
+          tmp = copy_board[cur_x][cur_y] + 100;
         }
         else
         {
-          next_state = 1;
+          tmp = copy_board[cur_x][cur_y] + 600;
         }
       }
 
-      if (board[next_x][next_y] == 0)
+      if (copy_board[next_x][next_y] > tmp || copy_board[next_x][next_y] == 0)
       {
-        int tmp;
-        if (next_state == 1)
-        {
-          tmp = copy_board[cur_x][cur_y][cur_state] + 600;
-        }
-        else
-        {
-          tmp = copy_board[cur_x][cur_y][cur_state] + 100;
-        }
-
-        if (copy_board[next_x][next_y][next_state] > tmp || copy_board[next_x][next_y][next_state] == 0)
-        {
-          copy_board[next_x][next_y][next_state] = tmp;
-          q.push(make_tuple(next_x, next_y, next_dir, next_state));
-        }
+        copy_board[next_x][next_y] = tmp;
+        q.push(make_tuple(next_x, next_y, next_dir));
       }
 
       if (next_x == n - 1 && next_y == n - 1)
       {
-        if (copy_board[next_x][next_y][next_state] < min_pay)
+        if (copy_board[next_x][next_y] < min_pay)
         {
-          min_pay = copy_board[next_x][next_y][next_state];
+          min_pay = copy_board[next_x][next_y];
           print(); //debugging
           return;
         }
@@ -134,8 +111,7 @@ int solution(vector<vector<int>> board)
     {
       if (board[i][j] == 1)
       {
-        copy_board[i][j][0] = -1;
-        copy_board[i][j][1] = -1;
+        copy_board[i][j] = -1;
       }
     }
   }
